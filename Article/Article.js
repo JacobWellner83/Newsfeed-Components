8d83f866b439ea1c0da1f7d81d3b36ef473e9cd8
JacobWellner83 <jcwellner83@gmail>
2019-04-16 22:35:22 -0500

    MVP Corrected

--- Article/Article.js
+++ Article/Article.js
@@ -5,11 +5,11 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelector('expandButton');
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle())
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this))
   }
 
   expandArticle() {
@@ -26,4 +26,5 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.article').forEach(article =>new Article(article));
+let articles = document.querySelectorAll('.article');
+articles.forEach(art => new Article(art));
